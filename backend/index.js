82868160411879359d014aca98d3a87648c2603f
Pankaj Yadav <Pankaj.Yadav@mastercard.com>
2024-08-29 13:51:40 +0530

    front end public directory added

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,14 @@
-const path = require('path');
 const express =  require('express');
+const cors = require('cors')
 const port = process.env.port || 3001;
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
-
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use('/images', express.static('public/images'));
 app.get('/api', (req, res) => {
     res.json({ message: 'Hello World!' });
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(port, () => {
